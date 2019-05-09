7afa3e4bca19ce44fd40d3b63a869abef437c66c
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-09 14:57:19 +0200

    Add two collapse elements to the first box

--- src/js/dist/index.js
+++ src/js/dist/index.js
@@ -1,47 +1,44 @@
 const slider = document.querySelector('.js-slider');
-const sliderBoxContent = slider.firstChild.textContent;
 const cards = document.querySelector('.js-cards');
-const cardsBoxContent = cards.firstChild.textContent;
-
-window.addEventListener('load', e => {
-  if (window.innerWidth >= 1200) {
-    slider.firstChild.textContent = '';
-    cards.firstChild.textContent = '';
-  } else {
-    slider.firstChild.textContent = sliderBoxContent;
-    cards.firstChild.textContent = cardsBoxContent;
-  }
-});
-
-window.addEventListener('resize', e => {
-  if (window.innerWidth >= 1200) {
-    slider.firstChild.textContent = '';
-    cards.firstChild.textContent = '';
-  } else {
-    slider.firstChild.textContent = sliderBoxContent;
-    cards.firstChild.textContent = cardsBoxContent;
-  }
-});
-
 const modal = document.querySelector('.js-modal');
+const collapse = document.querySelector('.js-collapse');
+const sliderBoxContent = slider.firstChild.textContent;
+const cardsBoxContent = cards.firstChild.textContent;
 const modalBoxContent = modal.firstChild.textContent;
+const collapseBoxContent = collapse.firstChild.textContent;
 
 window.addEventListener('load', e => {
   if (window.innerWidth >= 1200) {
+    slider.firstChild.textContent = '';
+    cards.firstChild.textContent = '';
     modal.firstChild.textContent = '';
+    collapse.firstChild.textContent = '';
   } else {
+    slider.firstChild.textContent = sliderBoxContent;
+    cards.firstChild.textContent = cardsBoxContent;
     modal.firstChild.textContent = modalBoxContent;
+    collapse.firstChild.textContent = collapseBoxContent;
   }
 });
 
 window.addEventListener('resize', e => {
   if (window.innerWidth >= 1200) {
+    slider.firstChild.textContent = '';
+    cards.firstChild.textContent = '';
     modal.firstChild.textContent = '';
+    collapse.firstChild.textContent = '';
   } else {
+    slider.firstChild.textContent = sliderBoxContent;
+    cards.firstChild.textContent = cardsBoxContent;
     modal.firstChild.textContent = modalBoxContent;
+    collapse.firstChild.textContent = collapseBoxContent;
   }
 });
 
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(function() {
+  $('[data-toggle="popover"]').popover();
 });
